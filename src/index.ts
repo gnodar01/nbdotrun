dd68a9704dbf3669f762c0d92db83378b6b76bfe
Nodar Gogoberidze <gnodar01@gmail.com>
2025-04-13 18:46:17 -0400

    Add notebook and cells packages

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,33 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import {
+  INotebookTracker,
+  NotebookPanel,
+  NotebookActions
+} from '@jupyterlab/notebook';
+import { CodeCellModel } from '@jupyterlab/cells';
+
+function throttle(fn: () => void, delay: number) {
+  let timeout: number | null = null;
+  return () => {
+    if (timeout) {
+      return;
+    }
+    timeout = window.setTimeout(() => {
+      fn();
+      timeout = null;
+    }, delay);
+  };
+}
 
 /**
  * Initialization data for the nbdotrun extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'nbdotrun:plugin',
-  description: 'A JupyterLab extension that will listen for code cell changes and run if ending in dot (`.`).',
+  description:
+    'A JupyterLab extension that will listen for code cell changes and run if ending in dot (`.`).',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension nbdotrun is activated!');
